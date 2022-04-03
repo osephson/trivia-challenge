310e0fbf455b11838ac0893de61d54ef0aea7455
osefson <osefson@gmail.com>
2022-04-03 11:46:35 -0400

    Fixed api url from env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,8 @@
 import { IQResponse } from '../interfaces';
 
 export const getQuestions = async (): Promise<IQResponse> => {
-  const response = await fetch(
-    'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
-  );
+  const url = process.env.REACT_APP_API_URL;
+  const response = await fetch(url as RequestInfo);
   const result = await response.json();
   return result;
 };
